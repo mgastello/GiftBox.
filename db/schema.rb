ffd147dc3b35142f5c26704c92a820d6ee28684f
Michael Gastello <michaelgastello@aol.com>
2024-09-26 22:39:33 -0400

    changed rails version from 7.2 to 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_26_184647) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_26_184647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,4 +25,5 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_26_184647) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
   end
+
 end
